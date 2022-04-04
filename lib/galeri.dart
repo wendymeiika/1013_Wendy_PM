cdde1f2f7f2e63c4a7cc4312dfdcb509c089eb02
FlutLab.io <>
2022-04-04 19:40:22 +0000

    Autosave

--- lib/galeri.dart
+++ lib/galeri.dart
@@ -15,7 +15,7 @@ class galeri extends StatelessWidget {
     'NCT Dream Renjun dikenal sebagai salah satu member NCT yang paling estetik',
     'NCT Dream Jeno merupakan salah satu member NCT yang kehadirannya sudah dikenal sejak ia masih remaja.',
     'NCT Dream Haechan diketahui sebagai salah satu idol yang memiliki otak yang cerdas dan kreatif',
-    'NCT Dream Jaemin Paras Jaemin yang menawan ini selalu bisa mencuri perhatian para NCTzen, terutama bagi para noona yang mengidolakan Jaemin',
+    'NCT Dream Jaemin Paras Jaemin yang menawan ini selalu bisa mencuri perhatian para NCTzen',
     'NCT Dream Chonle dikenal sebagai Chenle, adalah seorang penyanyi, penulis lagu, penari dan pemeran Tiongkok yang berbasis di Seoul, Korea Selatan.',
     'NCT Dream Jisung seorang penyanyi Korea Selatan yang berada di bawah naungan SM Entertainment.',
     'NCT Dream Mark Mark Lee, lebih dikenal dengan nama Mark adalah rapper, dancer, dan penyanyi berkebangsaan Kanada'
