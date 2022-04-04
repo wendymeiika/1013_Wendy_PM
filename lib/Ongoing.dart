f8b209e218554b752d03c81d0e429c80c75d83f0
FlutLab.io <>
2022-04-04 18:35:38 +0000

    Autosave

--- lib/Ongoing.dart
+++ lib/Ongoing.dart
@@ -2,49 +2,43 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Ongoing extends StatelessWidget {
-  final List<String> nama = [
-    'SPY X FAMILY',
-    'The Rising of the Shield Hero ',
-    'Yuusha Yamemasu',
-    'Komi Cant Communication (Season 2)',
-    'Tomodachi Game',
-    'Kaguya-sama: Love is War (Season 3)',
-    'BanG Dream! Poppin’ Dream!',
-    'Tokyo 24th Ward',
-    'Slow Loop',
-    'Aharen-san wa Hakarenai',
+  final List<String> gambar = [
+    "asset/image/DREAM 1.png",
+    "asset/image/DREAM 2.png",
+    "asset/image/DREAM 3.png",
+    "asset/image/DREAM 4.png",
+    "asset/image/DREAM 5.png",
+    "asset/image/DREAM 6.png",
+    "asset/image/DREAM 7.png"
   ];
 
-  final List<String> studio = [
-    'Wit Studio, CloverWorks',
-    'Kinema Citrus, DR Movie',
-    'EMT Squared',
-    'OLM',
-    'Okuruto Noboru',
-    'A-1 Pictures',
-    'Sanzigen ',
-    'CloverWorks',
-    'Connect',
-    'Felix Film',
+  final List<String> sub = [
+    'Life Is Still Going On - MV',
+    'Rainbow - MV',
+    'Chewing Gum - MV',
+    'My First and Last - MV',
+    'Dear Dream - MV',
+    'Go - MV',
+    'Trigger The Fever - MV'
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: ListView.builder(
-        itemCount: nama.length,
+        itemCount: gambar.length,
         itemBuilder: (context, index) {
           return Card(
             child: ListTile(
-              title: Text(
-                nama[index],
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(studio[index], style: TextStyle(color: Colors.lightBlueAccent, fontWeight: FontWeight.bold, fontSize: 20)),
+              title: ClipRRect(
+                  child: Image.asset(
+                gambar[index],
+                width: 300,
+                height: 200,
+                fit: BoxFit.cover,
+              )),
+              subtitle: Text(sub[index], style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 15)),
             ),
           );
         },
