c2bdd4c4bcb0bd9fc814a19f377a861f5bdebfaa
FlutLab.io <>
2022-04-04 12:43:43 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class BelajarAppBar extends StatelessWidget {
                         fontSize: 20.0,
                       )),
                   background: Image(
-                    image: AssetImage('asset/image/DREAM 1.png'),
+                    image: AssetImage('asset/image/bkn.jpeg'),
                     fit: BoxFit.cover,
                   ),
                 ),
